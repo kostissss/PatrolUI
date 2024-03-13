7cc70834fed1d43f6cf26530c2ffcaa86689e3c2
kostis <terracominterns2024@gmail.com>
2024-03-13 15:39:15 +0200

    fixing styles and adding dialogs to dialog folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DialogModule } from '@syncfusion/ej2-angular-popups';
-import { CreatePartnerAccountModalComponent } from './components/create-partner-account-modal/create-partner-account-modal.component';
+import { CreatePartnerAccountModalComponent } from './components/dialogs/create-partner-account-modal/create-partner-account-modal.component';
 import { FormsModule } from '@angular/forms';
 import { FormsDirectiveDirective } from './forms-directive.directive';
 import {CheckBoxModule} from '@syncfusion/ej2-angular-buttons';
 import {DropDownListModule} from '@syncfusion/ej2-angular-dropdowns';
-import { CreateNewAccountComponent } from './components/create-new-account/create-new-account.component';
+import { CreateNewAccountComponent } from './components/dialogs/create-new-account/create-new-account.component';
 import { DateTimePickerModule } from '@syncfusion/ej2-angular-calendars';
-import { ManagePartnerAccountModalComponent } from './components/manage-partner-account-modal/manage-partner-account-modal.component';
+import { ManagePartnerAccountModalComponent } from './components/dialogs/manage-partner-account-modal/manage-partner-account-modal.component';
 
 @NgModule({
   declarations: [
