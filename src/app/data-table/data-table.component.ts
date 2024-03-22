9b885ea41fe964dfb4c3b819ba2c26bea66c581b
Vantsis <vantsisq@gmail.com>
2024-03-22 13:58:22 +0200

    Header(dropdown need work) ready, ToolBar ready Delete, Reload, Search (with some css details), DataTable ready and count notifications ready.

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MyNotification, data } from '../shared/datasource';
-import { ToolbarItems, EditSettingsModel, SelectionSettingsModel } from '@syncfusion/ej2-angular-grids';
+import { ClickEventArgs } from '@syncfusion/ej2-navigations';
+import { ToolbarItems, EditSettingsModel, SelectionSettingsModel, GridComponent } from '@syncfusion/ej2-angular-grids';
 
 
 @Component({
@@ -13,31 +14,48 @@ export class DataTableComponent implements OnInit {
   public data?: MyNotification[];
   public editSettings?: EditSettingsModel;
 
+  @ViewChild('grid')
+  public grid: GridComponent;
+
+  public notificationCount: number = 0;
   public orderIDRules?: MyNotification;
   public customerIDRules?: MyNotification;
   public freightRules?: MyNotification;
   public toolbarOptions?: ToolbarItems[] | any;
   public selectionOptions?: SelectionSettingsModel;
+  public toolbar?: ToolbarItems[] | object;
 
   ngOnInit(): void {
     this.data = data;
+    this.notificationCount = this.data.length;
     this.selectionOptions = { mode: 'Row',  type: 'Single' };
     this.editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Dialog' };
-    this.toolbarOptions = [{ text: 'reload'},'Search',{ text: 'Add new Notification'}, { text: 'View Notification Details'}, 'Delete', { text: 'Assign To Partners'},];
+    this.toolbarOptions = [
+      { prefixIcon: 'e-refresh'},
+      { text: 'Add new Notification'},
+      { text: 'View Notification Details'}, 
+      { text: 'Delete Notiication'}, 
+      { text: 'Assign To Partners'},
+      'Search',
+      ];
   }
 
-  toolbarClick(args: Object | any): void {
-    if (args.item.text === 'Add new Notification') {
-      alert("ADD TEST");
+  clickHandler(args: ClickEventArgs): void {
+    if (args.item.prefixIcon === 'e-refresh') {
+      this.reloadPage();
     }
-    if (args.item.text === 'View Notification Details') {
-      alert("NOTIFICATIONS TEST");
+    if (args.item.text === 'Add new Notification') {
+      alert("ADD NOTIFICATION TEST");
+    }
+    if (args.item.text === 'View Notification Details') { 
+      alert("VIEW NOTIFICATIONS TEST");
+    }
+    if (args.item.text === 'Delete Notiication') { 
+      var selectedRecord = this.grid.getSelectedRecords()[0];
+      this.grid.deleteRecord(selectedRecord as string);
     }
     if (args.item.text === 'Assign To Partners') {
       alert("ASSIGN PARTNERS TEST");
-    }
-    if (args.item.text === 'reload') {
-      this.reloadPage();
     }
   }
 
