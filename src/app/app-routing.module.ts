87a941cfb8b695a3f3f88ffd6a7919d4d4b640b3
kostis <terracominterns2024@gmail.com>
2024-04-02 11:01:06 +0300

    added an authenticationGuard! Commented in app-routing.module to not bother local development

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuardGuard } from './guards/auth-guard.guard';
 
 const routes: Routes = [
  
-  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule),/*canActivate: [authGuardGuard]*/ }, 
  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
- { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
+ { path: '', redirectTo: '/home', pathMatch: 'full',/*canActivate: [authGuardGuard]*/ }, // Default route
  { path: ':id', redirectTo: '/login', pathMatch: 'full' }, // Default route
 ];
 
