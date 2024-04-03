01a831634cc944e7888b1db3ea3073217ad71ab8
Vantsis <vantsisq@gmail.com>
2024-04-03 14:39:07 +0300

    Merge branch 'Notifications' of https://gitlab.byterracom.com/qrpatrol/software/qr-patrol-admin-angular-pages/ui into Notifications

--- src/app/NotificationGrid/data-table/add-notification-dialog/add-notification-dialog.component.ts
+++ src/app/NotificationGrid/data-table/add-notification-dialog/add-notification-dialog.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ToolbarItems } from '@syncfusion/ej2-angular-grids';
-import { DialogComponent, ResizeDirections } from '@syncfusion/ej2-angular-popups';
-import { EmitType} from '@syncfusion/ej2-base';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-add-notification-dialog',
@@ -10,91 +6,5 @@ import { EmitType} from '@syncfusion/ej2-base';
   styleUrl: './add-notification-dialog.component.css'
 })
 export class AddNotificationDialogComponent {
-
-  notificationTitle = '';
-  notificationMessage = '';
-  
-
-
-  ngOnInit(): void {
-    this.toolbar = [
-      { prefixIcon: 'e-refresh'},
-      { text: 'Add new Notification', tooltipText: 'Add', id: 'Add',},
-      { text: 'View Notification Details', tooltipText: 'Edit', id: 'Edit',disabled: true}, 
-      { text: 'Delete Notification', tooltipText: 'Delete', id: 'Delete',disabled: true}, 
-      { text: 'Assign To Partners', tooltipText: 'Assign', id: 'Assign',disabled: true},
-      'Search',
-    ];
-  }
-
-
-  
-  public toolbar?: ToolbarItems[] | object;
-
-  
-    
-
-  // DIALOG CONFIG
-    public dialogAnimationSettings: Object = { effect: 'SlideTop', duration: 600};
-    public dialogResizeDirections: ResizeDirections[] = [
-      'All'
-    ];
-    public dialogPosition: Object = {
-      X: 'center', Y: 'center'
-    };
-  
-    
-    @ViewChild('dialogcomponent') dialogObject! : DialogComponent
-    public dialogVisibility : Boolean = false;
-    public onOpenDialog = (): void => {
-      this.dialogObject.show();
-    };
-  
-    public hideDialog: EmitType<object> = () => {
-       this.dialogObject.hide();
-    }
-  
-
-    // DIALOG BUTTONS
-    public dialogButton: Object = [
-      {
-        'click': this.hideDialog.bind(this),
-        buttonModel : {
-          content: 'Cancel',
-          //position: 'BottomLeft',
-          // target: '.e-footer-content', // Ορίστε το target ως '.e-footer-content'
-          //align: 'Left' // Τοποθετήστε το κουμπί στην αριστερή πλευρά
-          cssClass: 'cancel-button'
-        }
-      },
-      // {buttonModel : {
-      //   content: 'Save template',
-      //   //align : 'left'
-      // }
-      // },
-      {
-        'click': this.onSaveTemplate.bind(this), // Call onSaveTemplate when clicked
-        buttonModel : {
-          content: 'Save template',
-          cssClass: 'previewBtn'
-        }
-      },
-      {buttonModel : {
-        content: 'send',
-        cssClass: 'BtnBlue pt-2 '
-      }
-      }
-  
-    ];
-  
-    
-    
-  
-    
-  
-    onSaveTemplate() {
-      this.notificationMessage = "the users notificarion"
-    }
-
 
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -8,12 +8,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '../app-routing.module';
 import { DialogModule } from '@syncfusion/ej2-angular-popups';
 import { FormsModule } from '@angular/forms';
-import { CheckBoxModule } from '@syncfusion/ej2-angular-buttons';
-import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
-import { DateTimePickerModule } from '@syncfusion/ej2-angular-calendars';
+import { ButtonModule, CheckBoxModule } from '@syncfusion/ej2-angular-buttons';
+import { AutoCompleteModule, DropDownListModule, MultiSelectModule } from '@syncfusion/ej2-angular-dropdowns';
+import { DatePickerAllModule, DateTimePickerModule, TimePickerModule } from '@syncfusion/ej2-angular-calendars';
 import { HttpClientModule } from '@angular/common/http';
 import { DropDownButtonModule } from '@syncfusion/ej2-angular-splitbuttons';
-import { AccordionModule, MenuModule, SidebarModule } from '@syncfusion/ej2-angular-navigations';
+import { AccordionModule, MenuModule, SidebarModule, ToolbarModule } from '@syncfusion/ej2-angular-navigations';
 import { ListViewModule } from '@syncfusion/ej2-angular-lists';
 import { CreateNewAccountComponent } from '../components/dialogs/create-new-account/create-new-account.component';
 import { CreatePartnerAccountModalComponent } from '../components/dialogs/create-partner-account-modal/create-partner-account-modal.component';
@@ -24,7 +24,14 @@ import { HelpDropDownButtonComponent } from '../components/dropdowns/help-drop-d
 import { AccountChangeDropDownComponent } from '../components/dropdowns/account-change-drop-down/account-change-drop-down.component';
 import { ChangeUsernameDialogComponent } from '../components/dialogs/change-username-dialog/change-username-dialog.component';
 import { ChangeLanguageDialogComponent } from '../components/dialogs/change-language-dialog/change-language-dialog.component';
-
+import { DataTableComponent } from '../NotificationGrid/data-table/data-table.component';
+import { NotifHeaderComponent } from '../NotificationGrid/header/header.component';
+import { AddNotificationDialogComponent } from '../NotificationGrid/data-table/add-notification-dialog/add-notification-dialog.component';
+import { GridModule, PagerModule } from '@syncfusion/ej2-angular-grids';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TextBoxModule } from '@syncfusion/ej2-angular-inputs';
+import { RouterModule } from '@angular/router';
+import { GridComponent } from '../NotificationGrid/shared/grid/grid.component';
 
 @NgModule({
   declarations: [
@@ -40,6 +47,11 @@ import { ChangeLanguageDialogComponent } from '../components/dialogs/change-lang
     ChangeUsernameDialogComponent,
     ChangeLanguageDialogComponent,
     CreatePartnerAccountModalComponent,
+    DataTableComponent,
+    NotifHeaderComponent,
+    AddNotificationDialogComponent,
+    GridComponent,
+
 
   ],
   imports: [
@@ -58,6 +70,19 @@ import { ChangeLanguageDialogComponent } from '../components/dialogs/change-lang
     ListViewModule,
     AccordionModule,
     SidebarModule,
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    GridModule,
+    ButtonModule,
+    ToolbarModule,
+    DatePickerAllModule,
+    TimePickerModule,
+    TextBoxModule,
+    MultiSelectModule,
+    AutoCompleteModule,
+    FontAwesomeModule,
+    PagerModule
   ]
 })
 export class HomeModule { }
