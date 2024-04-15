28806c003bc40a027e927256a691b72a6746bd38
Vantsis <vantsisq@gmail.com>
2024-04-15 16:26:25 +0300

    frond end with local grid ready with css styles. missing buttons dialog and backend DB

--- src/app/components/companies/companies.component.ts
+++ src/app/components/companies/companies.component.ts
@@ -3,6 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { ClickEventArgs } from '@syncfusion/ej2-navigations';
 import { ToolbarItems, EditSettingsModel, SelectionSettingsModel, GridComponent } from '@syncfusion/ej2-angular-grids';
 import {PageSettingsModel } from '@syncfusion/ej2-angular-grids';
+import { data } from './datasource';
 
 @Component({
   selector: 'app-companies',
@@ -10,6 +11,7 @@ import {PageSettingsModel } from '@syncfusion/ej2-angular-grids';
   styleUrl: './companies.component.css'
 })
 export class CompaniesComponent implements OnInit {
+  public data!: object[];
   public buttonStatus : boolean = true;
   public editSettings?: EditSettingsModel;
   public companiesCount: number = 0;
@@ -27,7 +29,8 @@ export class CompaniesComponent implements OnInit {
   constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
-
+    this.data = data;
+    this.companiesCount = this.data.length;
     this.selectionOptions = { mode: 'Row',  type: 'Single' };
     this.editSettings = {allowDeleting: true, mode: 'Dialog',};
     this.toolbar = [
@@ -37,6 +40,7 @@ export class CompaniesComponent implements OnInit {
       { text: 'Export To Excel', tooltipText: 'Edit', id: 'Export',disabled: false}, 
       { text: 'Reset Password', tooltipText: 'Edit', id: 'Reset',disabled: false},
       { text: 'Change Plan', tooltipText: 'Edit', id: 'Edit', disabled: true},
+      'Search'
 
     ];
   }
